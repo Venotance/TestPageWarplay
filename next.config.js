d7937a9355367d2c51b1c2f1db77b55ec44a1d39
Stanislav Volkov <57898907+Venotance@users.noreply.github.com>
2023-03-25 18:47:25 +0300

    fix: next optimizer

--- next.config.js
+++ next.config.js
@@ -4,12 +4,16 @@ const withPlugins = require('next-compose-plugins');
 const nextConfiguration = {
   reactStrictMode: true,
   swcMinify: true,
+  images: {
+    loader: 'akamai',
+    path: '',
+  },
   // images: {
   //   domains: ['localhost'], // Сюда добавить домен, с которого нужно получать картинки на продакшене
   // },
   sassOptions: {
     includePaths: [path.join(__dirname, './src/shared/assets/styles')],
-    prependData: `@import "_global-import.scss";`
+    prependData: `@import "_global-import.scss";`,
   },
   typescript: {
     // !! WARN !!
